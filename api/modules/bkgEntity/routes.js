7d7800b6297e2a78835bd6c917000baa0fd8b54c
GKampitakis <csd3142@gmail.com>
2018-10-19 22:56:07 +0300

    a working state of bookings

--- api/modules/bkgEntity/routes.js
+++ api/modules/bkgEntity/routes.js
@@ -1,10 +1,10 @@
 const { router } = require("../../utils");
 const bkgEntityFunctions = require("./functions");
 
-// TODO: Implement these
-
 router.post("/bkgentities", bkgEntityFunctions.create);
 router.put("/bkgentities/:bId", bkgEntityFunctions.update);
 router.delete("/bkgentities/:bId", bkgEntityFunctions.delete);
 router.get("/bkgentities/:bId", bkgEntityFunctions.retrieve);
 router.get("/bkgentities", bkgEntityFunctions.retrieveAll);
+
+//TODO: an availability route
--- api/modules/booking/functions.js
+++ api/modules/booking/functions.js
@@ -2,16 +2,120 @@ const _ = require("lodash");
 const {
   bookingModel
 } = require("../../models");
+const {
+  bkgentityModel
+} = require("../../models")
 
 const {
   mapper
 } = require("../../middleware").mapper;
 
-const create = (req, res, next) => {
-  if (req.body) {
-    res.send('create');
+var moment = require('moment');
+const utils = require("../../utils");
+
+const create = (req, res, next) => { //FIXME: Not let past dates
+  var found = false;
+  if (req.body.bkgEntityId && req.body.patientId && req.body.timeslot && req.body.date) {
+    bkgentityModel.findById(req.body.bkgEntityId, (error, bgkentity) => {
+      if (error) next(error);
+      else if (!bgkentity) next("Not Found");
+      else {
+        Object.getOwnPropertyNames(bgkentity.days).forEach((item) => {
+          if (item === req.body.date) {
+            found = true;
+            let availability = bgkentity.days[item];
+            console.log(availability);
+            //Here the date exists initialized
+            var bookingslot = [];
+            bookingslot = bookingslot.concat(utils.helper.findObjectByKey(availability, 'slot', utils.helper.toHHMMSS(moment.duration(req.body.timeslot).asSeconds() + bgkentity.frequency * 60)));
+            if (bookingslot.length === 0) {
+              next("Not available Slot");
+            } else {
+              //here we have the slot and we are going to see if the numbers fit
+              for (var i = 0; i < availability.length; i++) {
+                if (bookingslot[0].slot === availability[i].slot) {
+                  if (availability[i].availability - 1 < 0) { //CAUTION Hard copy every booking has 1 "value" of availability
+                    next("No availability");
+                  } else availability[i].availability = availability[i].availability - 1;
+                }
+              }
+            }
+            bgkentity.days[req.body.date] = availability;
+            bgkentity.markModified('days');
+            bgkentity.save((error) => { //here check that the controll ends at the no availability
+              if (error) next(error);
+              else {
+                const booking = new bookingModel({
+                  bkgEntityId: req.body.bkgEntityId,
+                  patientId: req.body.patientId,
+                  timeslot: req.body.timeslot,
+                  date: req.body.date
+                })
+                booking.save((error, data) => {
+                  if (error) next(error);
+                  else {
+                    res.status(201);
+                    res.locals.data = mapper(data, "booking");
+                    next();
+                  }
+                })
+              }
+            })
+          }
+        });
+        if (found) return;
+        else {
+          let opening = moment.duration(bgkentity.opening).asSeconds();
+          let closing = moment.duration(bgkentity.closing).asSeconds();
+          var slots = Math.round(Math.round((closing - opening) / 60) / bgkentity.frequency);
+          let availability = [];
+          for (var i = 0; i < slots; i++) {
+            availability.push({
+              slot: utils.helper.toHHMMSS(opening + i * bgkentity.frequency * 60),
+              availability: bgkentity.availability
+            })
+          } // Up to here we ve got the availability
+
+          var bookingslot = [];
+          bookingslot = bookingslot.concat(utils.helper.findObjectByKey(availability, 'slot', utils.helper.toHHMMSS(moment.duration(req.body.timeslot).asSeconds() + bgkentity.frequency * 60)));
+          if (bookingslot.length === 0) {
+            next("Not available Slot");
+          } else {
+            //here we have the slot and we are going to see if the numbers fit
+            for (var i = 0; i < availability.length; i++) {
+              if (bookingslot[0].slot === availability[i].slot) {
+                if (availability[i].availability - 1 < 0) { //CAUTION Hard copy every booking has 1 "value" of availability
+                  next("No availability");
+                } else availability[i].availability = availability[i].availability - 1;
+              }
+            }
+            bgkentity.days[req.body.date] = availability;
+            bgkentity.markModified('days');
+            bgkentity.save((error) => { //here check that the controll ends at the no availability
+              if (error) next(error);
+              else {
+                const booking = new bookingModel({
+                  bkgEntityId: req.body.bkgEntityId,
+                  patientId: req.body.patientId,
+                  timeslot: req.body.timeslot,
+                  date: req.body.date
+                })
+                booking.save((error, data) => {
+                  if (error) next(error);
+                  else {
+                    res.status(201);
+                    res.locals.data = mapper(data, "booking");
+                    next();
+                  }
+                })
+              }
+            })
+          }
+        }
+      }
+    });
   } else {
-    next("Invalid Arguments"); //This error needs handling
+    next("Invalid Arguments");
   }
 };
 
@@ -28,11 +132,11 @@ const _delete = (req, res, next) => {
 };
 
 const retrieve = (req, res, next) => {
-  res.send('retrieve');
+
 };
 
 const retrieveAll = (req, res, next) => {
-  res.send('retrieveAll');
+
 };
 
 
